1e34223c29dc3f3348fa4a17e189a884f4d3d772
yccna <119087477+yccna@users.noreply.github.com>
2022-11-26 08:54:38 +0700

    Add all files to Project
    Hi Man,

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+    res.send('hello goo eng');
+})
+
+app.listen(port, () => {
+    console.log("listening on port  %d", port);
+})
